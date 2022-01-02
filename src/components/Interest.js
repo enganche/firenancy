3a0a83d8a5ead03dec030b90342206916a1ff74a
Anhtuanzzz <62417928+Anhtuanzzz@users.noreply.github.com>
2022-01-02 10:05:29 +0700

    change more

--- src/components/Interest.js
+++ src/components/Interest.js
@@ -8,33 +8,37 @@ export default function Interest() {
     const [finalMoney, setFinalMoney] = useState(0);
 
     function calculate() {
-        let final = currentMoney * Math.pow(term, rate);
+        let final = validate(currentMoney) * Math.pow(term, rate);
         setFinalMoney(final);
     }
 
     return (
         <div>
             <h1>Test</h1>
-            <input 
+            <input
+            className = 'interest'
             type = 'text'
             placeholder = 'Nhập số tiền gửi ban đầu'
             value = {currentMoney}
             onChange = {event => setCurrentMoney(event.target.value)}
             />
             <input 
+            className = 'interest'
             type = 'text'
             placeholder = 'Nhập lãi suất'
             value = {rate}
             onChange = {event => setRate(event.target.value)}
             />
             <input 
+            className = 'interest'
             type = 'text'
             placeholder = 'Nhập kì hạn'
             value = {term}
             onChange = {event => setTerm(event.target.value)}
             />
-            <button onClick={calculate}>Tính lãi đơn</button>
-            <p>{finalMoney}</p>
+            <button className = 'interest' onClick={calculate}>Tính lãi đơn</button>
+            <button className = 'interest' onClick={calculate}>Tính lãi kép</button>
+            <h1>{finalMoney}</h1>
         </div>
     );
 }
