da49114e069ea545165e9ba9f5d64085e02e5056
Anhtuanzzz <62417928+Anhtuanzzz@users.noreply.github.com>
2022-01-03 15:25:51 +0700

    release 0.1.0

--- src/components/Exchange.js
+++ src/components/Exchange.js
@@ -7,16 +7,16 @@ export default function Exchange() {
     const [result, setResult] = useState(0);
 
     useEffect(() => {
-        fetchData();
+        getData();
       }, []);
 
-    async function fetchData() {
+    async function getData() {
         const CURRENCY = ['VND', 'USD', 'EUR', 'BTC', 'ETC'];
         const API_KEY = '7ad3c800-615e-11ec-86a5-ede06f411e3e';
         const API_BASE = 'https://freecurrencyapi.net/api/v2/latest?apikey=';
         let res = await fetch(API_BASE + API_KEY + '&base_currency=' + CURRENCY[0]);
         let response = await res.json();
-        console.log(response);
+        //console.log(response);
         setRate(response.data.USD);
     }
 
@@ -54,8 +54,6 @@ export default function Exchange() {
             />
             <button className = 'exchange' oneClick = {reversedSubmit} >USD</button>
             <p>Tỉ lệ {rate}</p>
-            <p>1 VND đổi được {rate} USD</p>
-            <p>1 USD đổi được {1 / rate} VND</p>
         </div>
     );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
     return (
         <div className = 'footer'>
             <p>FireNancy - made by Anh Tuan</p>
+            <p>Github: https://github.com/Anhtuanzzz</p>
         </div>
     );
 }
